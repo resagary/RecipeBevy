a58e1762788c601a7adca785aaec0f21f3e99978
Resa Gary <resa.gary@yahoo.com>
2022-11-07 16:32:40 -0800

    updated shopping list form

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -8,6 +8,7 @@ import { Ingredient } from '../shared/ingredient.model';
 })
 export class ShoppingListService {
   ingredientsAdded = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Garlic', 1),
@@ -20,13 +21,31 @@ export class ShoppingListService {
     return this.ingredients.slice();
   }
 
+  getIngredient(index: number) {
+    return this.ingredients[index];
+  }
+
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsAdded.next(this.ingredients.slice());
+    this.onChangedShoppingList();
   }
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
+    this.onChangedShoppingList();
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.onChangedShoppingList();
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
+    this.onChangedShoppingList();
+  }
+
+  onChangedShoppingList() {
     this.ingredientsAdded.next(this.ingredients.slice());
   }
 }
